89eecfd7cc36e959f188f4f5e31e5c74f4f44ad0
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-03-06 12:11:24 -0500

    update connection port

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
+let sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
@@ -10,6 +10,7 @@ const sequelize = process.env.JAWSDB_URL
       dialectOptions: {
         decimalNumbers: true,
       },
+      port: 3306
     });
 
 module.exports = sequelize;
